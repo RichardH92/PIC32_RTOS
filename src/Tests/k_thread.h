750bc28b0a22e282eb87d0c2b0730d15ca3b8454
Richard Hart <richardhart92@gmail.com>
2016-06-24 18:23:44 -0400

    Started working on implementing context switching.

---
+++ src/Tests/k_thread.h
@@ -0,0 +1,43 @@
+#ifndef K_THREAD_H
+#define K_THREAD_H
+
+typedef tid_t unsigned int;
+typedef reg unsigned int;
+
+struct thread {
+	reg v0;
+	reg v1;
+	reg a[3];
+	reg t[10];
+	reg s[8];
+	reg k[2];
+	reg global_ptr;
+	reg stack_ptr;
+	reg frame_ptr;
+	reg ret_addr;
+	reg hi;
+	reg lo;
+	reg prog_counter;
+
+	char name[16];
+	
+	unsigned int magic = 0xDEADBEEF;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
